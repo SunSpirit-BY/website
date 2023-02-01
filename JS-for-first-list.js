436d267cab91f04d15932a5d631e0128a1fbfb98
Your Name <you@example.com>
2023-02-01 20:15:41 +0300

    подключил js, сделал кнопку.

---
+++ JS-for-first-list.js
@@ -0,0 +1,9 @@
+alert('JS подключён.');
+let But = document.querySelector('button');
+let Par = document.querySelector('.p2')
+But.onclick = function() {
+console.log('Кнопка нажата!');
+But.classList.remove('b1');
+But.classList.add('button2');
+Par.textContent='Стильно';
+}
