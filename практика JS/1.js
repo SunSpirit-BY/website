f02026f8da043eb1e0e811c394f1fd44e5c9aaf0
Your Name <you@example.com>
2023-02-15 20:51:48 +0300

    Js практику добавил

---
+++ практика JS/1.js
@@ -0,0 +1,28 @@
+//Задание 1 Вывести числа от 1 до 5 в консоль, используя все виды циклов(fow, while, do)
+let jopa=1;
+
+do{console.log(jopa);
+    jopa++;
+}
+while(jopa<6)
+
+for(let x=1;x<6;x++){
+    console.log(x)
+}
+
+let y=1;
+while(y<6){
+    console.log(y);
+y++;
+}
+//Задание 2 Верно ли, что последний вывод в консоль будет 0?
+let x1=8;
+while(x1){
+    console.log(x1);
+    x1--;
+}// Неверно, т.к. переменная x1 станет 0, что вернёт false.
+
+//переписать данный код на while
+for(let num = 0;num < 3;num++){
+        console.log(`Число:${num}`);
+}1` Q`
